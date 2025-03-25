7fbc4361b837e190ccee32f5529c75ff76847a23
Kishikawa Katsumi <kishikawakatsumi@mac.com>
2025-03-25 23:47:15 +0900

    Reformat Package.swift

--- Package.swift
+++ Package.swift
@@ -2,79 +2,79 @@
 import PackageDescription
 
 let package = Package(
-    name: "swiftregex",
-    platforms: [
-        .macOS(.v12)
-    ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-experimental-string-processing.git", branch: "main"),
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.114.0"),
-        .package(url: "https://github.com/vapor/leaf.git", from: "4.4.1"),
-    ],
-    targets: [
-        .executableTarget(
-            name: "DSLConverter",
-            dependencies: [
-                .product(name: "_StringProcessing", package: "swift-experimental-string-processing"),
-                .product(name: "_RegexParser", package: "swift-experimental-string-processing"),
-            ],
-            swiftSettings: [
-                .unsafeFlags(["-Xfrontend", "-disable-availability-checking"]),
-                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
-            ]
-        ),
-        .executableTarget(
-            name: "DSLParser",
-            dependencies: [
-                .product(name: "_StringProcessing", package: "swift-experimental-string-processing"),
-                .product(name: "_RegexParser", package: "swift-experimental-string-processing"),
-            ],
-            swiftSettings: [
-                .unsafeFlags(["-Xfrontend", "-disable-availability-checking"]),
-                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
-            ]
-        ),
-        .executableTarget(
-            name: "ExpressionParser",
-            dependencies: [
-                .product(name: "_StringProcessing", package: "swift-experimental-string-processing"),
-                .product(name: "_RegexParser", package: "swift-experimental-string-processing"),
-            ],
-            swiftSettings: [
-                .unsafeFlags(["-Xfrontend", "-disable-availability-checking"]),
-                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
-            ]
-        ),
-        .executableTarget(
-            name: "Matcher",
-            dependencies: [
-                .product(name: "_StringProcessing", package: "swift-experimental-string-processing"),
-                .product(name: "_RegexParser", package: "swift-experimental-string-processing"),
-            ],
-            swiftSettings: [
-                .unsafeFlags(["-Xfrontend", "-disable-availability-checking"]),
-                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
-            ]
-        ),
-        .executableTarget(
-            name: "App",
-            dependencies: [
-                .product(name: "Vapor", package: "vapor"),
-                .product(name: "Leaf", package: "leaf"),
-            ],
-            swiftSettings: [
-                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
-            ]
-        ),
-        .testTarget(
-            name: "RegexTests", dependencies: [
-                .target(name: "DSLConverter"),
-                .target(name: "DSLParser"),
-                .target(name: "ExpressionParser"),
-                .target(name: "Matcher"),
-                .target(name: "App"),
-                .product(name: "XCTVapor", package: "vapor"),
-            ]
-        )
-    ]
+  name: "swiftregex",
+  platforms: [
+    .macOS(.v12)
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-experimental-string-processing.git", branch: "main"),
+    .package(url: "https://github.com/vapor/vapor.git", from: "4.114.0"),
+    .package(url: "https://github.com/vapor/leaf.git", from: "4.4.1"),
+  ],
+  targets: [
+    .executableTarget(
+      name: "DSLConverter",
+      dependencies: [
+        .product(name: "_StringProcessing", package: "swift-experimental-string-processing"),
+        .product(name: "_RegexParser", package: "swift-experimental-string-processing"),
+      ],
+      swiftSettings: [
+        .unsafeFlags(["-Xfrontend", "-disable-availability-checking"]),
+        .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release)),
+      ]
+    ),
+    .executableTarget(
+      name: "DSLParser",
+      dependencies: [
+        .product(name: "_StringProcessing", package: "swift-experimental-string-processing"),
+        .product(name: "_RegexParser", package: "swift-experimental-string-processing"),
+      ],
+      swiftSettings: [
+        .unsafeFlags(["-Xfrontend", "-disable-availability-checking"]),
+        .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release)),
+      ]
+    ),
+    .executableTarget(
+      name: "ExpressionParser",
+      dependencies: [
+        .product(name: "_StringProcessing", package: "swift-experimental-string-processing"),
+        .product(name: "_RegexParser", package: "swift-experimental-string-processing"),
+      ],
+      swiftSettings: [
+        .unsafeFlags(["-Xfrontend", "-disable-availability-checking"]),
+        .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release)),
+      ]
+    ),
+    .executableTarget(
+      name: "Matcher",
+      dependencies: [
+        .product(name: "_StringProcessing", package: "swift-experimental-string-processing"),
+        .product(name: "_RegexParser", package: "swift-experimental-string-processing"),
+      ],
+      swiftSettings: [
+        .unsafeFlags(["-Xfrontend", "-disable-availability-checking"]),
+        .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release)),
+      ]
+    ),
+    .executableTarget(
+      name: "App",
+      dependencies: [
+        .product(name: "Vapor", package: "vapor"),
+        .product(name: "Leaf", package: "leaf"),
+      ],
+      swiftSettings: [
+        .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
+      ]
+    ),
+    .testTarget(
+      name: "RegexTests", dependencies: [
+        .target(name: "DSLConverter"),
+        .target(name: "DSLParser"),
+        .target(name: "ExpressionParser"),
+        .target(name: "Matcher"),
+        .target(name: "App"),
+        .product(name: "XCTVapor", package: "vapor"),
+      ]
+    )
+  ]
 )
