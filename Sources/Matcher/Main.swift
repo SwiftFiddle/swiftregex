3abec9e3d9222948a5784907c435654ed022ff8e
Kishikawa Katsumi <kishikawakatsumi@mac.com>
2025-03-29 00:53:29 +0900

    Recovery from parse error

--- Sources/Matcher/Main.swift
+++ Sources/Matcher/Main.swift
@@ -29,7 +29,7 @@ struct Main {
 
 var standardError = FileHandle.standardError
 
-extension FileHandle : TextOutputStream {
+extension FileHandle: @retroactive TextOutputStream {
   public func write(_ string: String) {
     guard let data = string.data(using: .utf8) else { return }
     self.write(data)
