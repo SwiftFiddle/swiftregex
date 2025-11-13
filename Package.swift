00abe7c05c5b2c2c07f373acfac0b97bc08fb51a
Kishikawa Katsumi <kishikawakatsumi@mac.com>
2025-11-14 02:59:49 +0900

    repo

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     .macOS(.v12)
   ],
   dependencies: [
-    .package(url: "https://github.com/kishikawakatsumi/swift-experimental-string-processing.git", branch: "metrics"),
+    .package(url: "https://github.com/swiftlang/swift-experimental-string-processing.git", branch: "main"),
     .package(url: "https://github.com/vapor/vapor.git", from: "4.119.2"),
     .package(url: "https://github.com/vapor/leaf.git", from: "4.5.1"),
   ],
